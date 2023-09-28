2ba6011c3f62b03d7180cd4f955ee7ec04592e1e
Jana <128377430+jana423151@users.noreply.github.com>
2023-09-28 19:17:38 +0300

    3-purchase

--- src/route/index.js
+++ src/route/index.js
@@ -5,6 +5,79 @@ const router = express.Router()
 
 // ================================================================
 
+class Product {
+  static #list = []
+  static #count = 0
+
+  constructor(img, title, description, category, price, amount = 0,) {
+    this.id = ++Product.#count // Генеруемо унікальний id для товару
+    this.img = img
+    this.title = title
+    this.description = description
+    this.category = category
+    this.price = price
+    this.amount = amount 
+  }
+
+  static add = (...data ) => {
+    const newProduct = new Product(...data)
+    this.#list.push(newProduct)
+  }
+
+  static getList = () => {
+    return this.#list
+  }
+
+  static getById = (id) => {
+    return this.#list.find((product) => product.id === id)
+  }
+
+  static getRandomList = (id) => {
+    // Фільтруе товари, щоб вилучити той, з яким порівнюємо id
+    const filteredList = this.#list.filter(
+      (product) => product.id !== id,
+    )
+
+    // Відсортовуємо за допомогою math.random() та перемішуємо масив
+    const shuffledList = filteredList.sort(
+      () => Math.random() - 0.5,
+    )
+
+    // Повертаємо перші 3 елементи з перемішаного масиву
+    return shuffledList.slice(0, 3)
+  }
+}
+
+Product.add(
+  'https://picsum.photos/200/300',
+  "Комп'ютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel",
+  "Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux",
+  [
+    { id: 1, text: 'Готовий до відправки' },
+    { id: 2, text: 'Топ продажів' },
+  ],
+  27000,
+  10,
+)
+
+Product.add(
+  'https://picsum.photos/200/300',
+  "Комп'ютер ARTLINE Gaming by ASUS TUF v119 (TUFv119)",
+  "Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС",
+  [{ id: 2, text: 'Топ продажів' }],
+  17000,
+  10,
+)
+
+Product.add(
+  'https://picsum.photos/200/300',
+  "Комп'ютер Artline Gaming (X43v31) AMD Ryzen 5 3600/",
+  "AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС",
+  [{ id: 1, text: 'Готовий до відправки' }],
+  40000,
+  10,
+)
+
 // router.get Створює нам один ентпоїнт
 
 // ↙️ тут вводимо шлях (PATH) до сторінки
@@ -12,14 +85,133 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   // ↙️ cюди вводимо назву файлу з сontainer
-  res.render('index', {
+  res.render('purchase-index', {
     // вказуємо назву папки контейнера, в якій знаходяться наші стилі
-    style: 'index',
+    style: 'purchase-index',
+
+    data: {
+      list: Product.getList(),
+    },
   })
   // ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/purchase-product', function (req, res) {
+  const id = Number (req.query.id)
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('purchase-product', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'purchase-product',
+
+    data: {
+    list: Product.getRandomList(),
+    product: Product.getById(),
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ==============================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.get('/', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('alert', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'alert',
+
+    data: {
+      message: 'Успішне виконання дії',
+      info: 'замовлення успішно було створено',
+      link:'/test-path',
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/purchase-create', function (req, res) {
+  const id = Number(req.query.id)
+  const amount = Number(req.body.amount)
+
+  if (amount > 1) {
+    return res.render('alert', {
+    style: 'alert',
+
+    data: {
+    message: 'Помилка',
+    info: 'Неккоректна кількість товару',
+    link: `/purchase-product?id=${id}`,
+    },
+   })
+  }
+
+  const product = Product.getById(id)
+
+  if (product.amount < 1) {
+    return res.render('alert', {
+    style: 'alert',
+
+    data: {
+    message: 'Помилка',
+    info: 'Такої кількості товарів немає в наявності',
+    link: `/purchase-product?id=${id}`,
+    },
+   })
+  }
+
+  console.log(product.amount)
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('purchase-product', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'purchase-product',
+
+    data: {
+    list: Product.getRandomList(),
+    product: Product.getById(),
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
+
+// ==============================================================
+
+// router.get Створює нам один ентпоїнт
+
+// ↙️ тут вводимо шлях (PATH) до сторінки
+router.post('/purchase-submit', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  // ↙️ cюди вводимо назву файлу з сontainer
+  res.render('alert', {
+    // вказуємо назву папки контейнера, в якій знаходяться наші стилі
+    style: 'alert',
+
+    data: {
+      message: 'Успішно',
+      info: 'Замовлення створено',
+      link:'/purchase-list',
+    },
+  })
+  // ↑↑ сюди вводимо JSON дані
+})
 
 // Підключаємо роутер до бек-енду
 module.exports = router
